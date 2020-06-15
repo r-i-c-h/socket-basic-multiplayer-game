430d060985186d0d1d56bf85819baf211f652441
r-i-c-h <rich@richwerden.com>
2020-06-15 14:55:31 -0700

    renames server file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const socketio = require('socket.io');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
 app.use(express.static(`${__dirname}/../client`));
 
 const server = http.createServer(app);
@@ -13,10 +12,10 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
   console.log('New Connection');
-
   socket.emit('message', 'You are connected');
 
   socket.on('message', (txt) => io.emit('message', txt));
+  socket.on('turn', ({ x, y }) => io.emit('turn', { x, y }));
 });
 
 server.on('error', (err) => {
